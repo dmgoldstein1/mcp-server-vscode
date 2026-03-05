e78980081b03c97b1b648a59d304be44a7a0de91
dmgoldstein1 <96954562+dmgoldstein1@users.noreply.github.com>
2026-03-05 18:40:53 -0500

    Add asset composer snippet for loading external scripts and handling errors

---
+++ mcp-server-vscode - Codacy - Issues_files/env.js
@@ -0,0 +1,17 @@
+window.codacyEnv = {
+  codacyUrl: 'https://www.codacy.com',
+  codacyAppUrl: 'https://app.codacy.com',
+  codacyApiUrl: 'https://app.codacy.com',
+  codacyHeartbeatInterval: '30000',
+  sessionIdleTimeout: '4h',
+  environment: 'production',
+  appBasePath: '',
+  oAuthAppJiraClientId: 'PL0SCZWlKCHU3xtWiHrCqPvfEVsCHI2O',
+  documentation_version: 'latest',
+  segmentProjectWriteKey: '4sT1ml0BeKdR1RtrK5dSQmwxmvcUpYtL',
+  sentryDSN: 'https://19a0f49c7be442f497234cd9382f3c14@o375005.ingest.sentry.io/5193837',
+  launchDarklyClientSideId: '61ade2ec9a5a26130485865f',
+  stripePublicKey: 'pk_live_51ORzAMCmE6ulc7Kts9ppL3BumroSTwHh80a7pbHIBu1cTS9srReGIoAJx5iKvaoxznC50fIZnnRA8Y8K0ctlskYi00hdBXYXIj',
+  pagination_repositoriesLimit: '100',
+  entitiesLimitsStandardsMaxLimit: '30'
+}
